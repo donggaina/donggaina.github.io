c4093cdc1f8dfc45ecef5512911d6387a74afa62
donggaina <donggaina@126.com>
2016-03-18 08:38:13 +0800

    倒计时

--- qyer-Mobile/js/yqer.js
+++ qyer-Mobile/js/yqer.js
@@ -1,7 +1,7 @@
 Zepto(function () {
     getTime();
     function getTime() {
-        var endTime = new Date('2016/03/17 00:00:00');
+        var endTime = new Date('2016/03/21 00:00:00');
         var nowTime = new Date();
         var t = endTime.getTime() - nowTime.getTime();
         console.log("t:=="+t);
